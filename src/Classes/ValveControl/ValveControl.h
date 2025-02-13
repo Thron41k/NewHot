06a7d65fcce1e0b7959cd0d5869228239fe7f461
Timothy <Thron41k@gmail.com>
2025-02-13 16:33:02 +0800

    work in progress

--- src/Classes/ValveControl/ValveControl.h
+++ src/Classes/ValveControl/ValveControl.h
@@ -1,13 +1,11 @@
-#include "ValveControl.h"
+#include "..\StepperControl\StepperControl.h"
 
-ValveControl::ValveControl()
-  : _stepperControl(std::make_unique<StepperControl>()) {}
-
-int ValveControl::GetPositionFromPercent(short percent) const {
-  return _stepperControl->GetMaxPosition() * percent / 100;
-}
-
-void ValveControl::SetPercentage(short percent) {
-  percent = std::clamp(percent, static_cast<short>(0), static_cast<short>(100));
-  _stepperControl->GoToPosition(GetPositionFromPercent(percent));
-}
+class ValveControl {
+private:
+  std::unique_ptr<StepperControl> _stepperControl;
+  int GetPositionFromPercent(short percent) const;
+public:
+  ValveControl();
+  ~ValveControl() = default;
+  void SetPercentage(short percent);
+};
